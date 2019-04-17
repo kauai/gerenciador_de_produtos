464d8f2dda3ebd3291bb866f08cc9717f4e10935
Thiago kauai <html5ephp@gmail.com>
2019-04-17 14:36:11 -0300

    definindo rota produtos e categorias

--- src/componentes/Produtos.js
+++ src/componentes/Produtos.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import ProdutosHome from './ProdutosHome'
 
 export default class Produtos extends Component {
   render() {
     return (
-      <div>
-        <h1>Produtos</h1>
-      </div>
+      <>
+        <section className="produtos">
+            {/* //FUNCIONA TBM DESTA MANEIRA!!!
+              <Route exact={this.props.match.url} component={ProdutosHome}/> 
+            */}
+               <Route exact path={this.props.match.url} component={ProdutosHome}/> 
+        </section>
+        <asside className="categorias">
+            <h3>Categoria</h3>
+            <p>Link para categoria</p>
+        </asside>
+      </>
     )
   }
 }
---
+++ src/componentes/ProdutosHome.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+const ProdutosHome = () => 
+(
+    <div> 
+        <h1>Produtos Home</h1>
+    </div>
+)
+
+export default ProdutosHome
